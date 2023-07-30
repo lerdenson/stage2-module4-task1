3fc5fd1b64b5ebe32a14697ed09868afba752f84
Максим <Maxim.d.morozov@gmail.com>
2023-07-30 16:25:36 +0400

    first

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,20 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private volatile ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+
+    }
+
+    public ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return instance;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,35 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        Connection connection;
+        Properties props = new Properties();
+        try {
+            props.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(
+                    props.getProperty("db_url"),
+                    props.getProperty("user"),
+                    props.getProperty("password"));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+
+    }
 }
 
